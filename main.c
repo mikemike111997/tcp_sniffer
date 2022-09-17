689c34f1e0c4c420f981e95d7e7c3678f64532a1
Mykhailo Lohvynenko <mikemike111997@gmail.com>
2022-09-17 17:22:12 +0300

    sniff all available devices

--- main.c
+++ main.c
@@ -22,18 +22,19 @@
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <string.h>
-
+#include <pthread.h>
 #include "tcp_connection_info.h"
 
 
 enum STATUS_CODES
 {
     SUCCESS = 0,
-    INVALID_ARGV = 1,
-    PCAP_INITIALIZE_ERROR = 2
+    PCAP_INITIALIZE_ERROR = 1
 };
 
 static node_t* listHead;
+
+pthread_mutex_t lock;
 
 static void packetHandler(u_char* userData __attribute__((unused)),
                           const struct pcap_pkthdr* pkthdr __attribute__((unused)),
@@ -42,14 +43,12 @@ static void packetHandler(u_char* userData __attribute__((unused)),
     const struct ether_header* ethernetHeader = (struct ether_header*)packet;
     if (ntohs(ethernetHeader->ether_type) != ETHERTYPE_IP)
     {
-        fprintf(stderr, "Not an IP package, skipping it!\n");
         return;
     }
 
     const struct ip* ipHeader = (struct ip*)(packet + sizeof(struct ether_header));
     if (ipHeader->ip_p != IPPROTO_TCP)
     {
-        fprintf(stderr, "Not a TCP package, skipping it!\n");
         return;
     }
 
@@ -68,37 +67,33 @@ static void packetHandler(u_char* userData __attribute__((unused)),
             .handshakeSucceeded = 0
         };
      
+        pthread_mutex_lock(&lock);
         updateConnectionInfoList(&listHead, &info);
+        pthread_mutex_unlock(&lock);
     }
 }
 
-int main(int argC, char* argV[])
+static void* analyzeTrafficThreadFunct(void* devName)
 {
-    if (argC != 2)
-    {
-        fprintf(stderr, "Error: expected device name as an input param");
-        return INVALID_ARGV;
-    }
- 
-    char errbuf[PCAP_ERRBUF_SIZE] = {0}; // pcap error buffer
-    struct bpf_program fp;               // The compiled filter expression 
-    const char filter_exp[] = "tcp ";    // The filter expression
-    bpf_u_int32 mask = 0;                // The netmask of our sniffing device
-    bpf_u_int32 net = 0;                 // The IP of our sniffing device
+    char errbuf[PCAP_ERRBUF_SIZE] = {0};    // pcap error buffer
+    struct bpf_program fp;                  // The compiled filter expression 
+    static const char filter_exp[] = "tcp"; // The filter expression
+    bpf_u_int32 mask = 0;                   // The netmask of our sniffing device
+    bpf_u_int32 net = 0;                    // The IP of our sniffing device
 
-    char* dev = argV[1];
+    const char* dev = (const char*)devName;
     if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1)
     {
         fprintf(stderr, "pcap_lookupnet failed for device %s\n", dev);
         fprintf(stderr, "%s\n", errbuf);
-        return PCAP_INITIALIZE_ERROR;
+        return NULL;
     }
     else
     {
         struct in_addr ip_addr = {.s_addr = net};
         struct in_addr ip_mask_addr = {.s_addr = mask};
-        printf("Device: %s\n", dev);
-        printf("Network: %s\n", inet_ntoa(ip_addr));
+        printf("Device: %s ", dev);
+        printf("Network: %s ", inet_ntoa(ip_addr));
         printf("Mask = %s\n", inet_ntoa(ip_mask_addr));
     }
 
@@ -106,19 +101,19 @@ int main(int argC, char* argV[])
     if (handle == NULL)
     {
         fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-        return PCAP_INITIALIZE_ERROR;
+        return NULL;
     }
 
     if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1)
     {
         fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-        return PCAP_INITIALIZE_ERROR;
+        return NULL;
     }
 
     if (pcap_setfilter(handle, &fp) == -1)
     {
         fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-        return PCAP_INITIALIZE_ERROR;
+        return NULL;
     }
 
     pcap_loop(handle, -1, packetHandler, NULL);
@@ -126,6 +121,80 @@ int main(int argC, char* argV[])
     // clean up resources
     pcap_close(handle);
     pcap_freecode(&fp);
+
+    return NULL;
+}
+
+static uint8_t isCapturebleDevie(pcap_if_t* it)
+{
+    pcap_addr_t *dev_addr; //interface address that used by pcap_findalldevs()
+
+    /* check if the device captureble*/
+    for (dev_addr = it->addresses; dev_addr != NULL; dev_addr = dev_addr->next) {
+        if (dev_addr->addr->sa_family == AF_INET && dev_addr->addr && dev_addr->netmask) {
+           return 1;
+        }
+    }
+    return 0;
+}
+
+static size_t coundDevicesAvaiilable(pcap_if_t* it)
+{
+    size_t res = 0;
+    while (it)
+    {
+        if (isCapturebleDevie(it))
+            ++res;
+        it = it->next;
+    } 
+
+    return res;
+}
+
+int main(void)
+{
+    char errbuf[PCAP_ERRBUF_SIZE] = {0}; // pcap error buffer
+    pcap_if_t *it = NULL;
+
+    if(pcap_findalldevs(&it, errbuf) == 0)
+    {
+        pcap_if_t* current = it;
+        const size_t availableDevicesCount = coundDevicesAvaiilable(it);
+
+        printf("Number of available devices for TPC traffic sniffing: %lu\n", availableDevicesCount);
+
+        pthread_t threads[availableDevicesCount];
+        memset(&threads, 0, sizeof(threads));
+
+        // spawn worker threads
+        for (size_t i = 0; i < availableDevicesCount; ++i)
+        {
+            if (isCapturebleDevie(it))
+            {
+                if (pthread_create(&threads[i], NULL, &analyzeTrafficThreadFunct, (void*)current->name))
+                    fprintf(stderr, "Thead creation failed for the dev %s", current->name);
+            }
+
+            current = current->next;
+        }
+
+        // join threads
+        for (size_t i = 0; i < availableDevicesCount; ++i)
+        {
+            if (pthread_join(threads[i], NULL))
+                fprintf(stderr, "Thead join failed!");
+        }
+
+        // clear resources
+        pcap_freealldevs(it);
+    }
+    else
+    {
+        fprintf(stderr, "pcap_findalldevs failed.\n");
+        fprintf(stderr, "%s\n", errbuf);
+        return PCAP_INITIALIZE_ERROR;
+    }
+
     deleteList(&listHead);
 
     return SUCCESS;
